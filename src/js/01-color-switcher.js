d84b693378f472f367e44c7e3c8eadb975d01e90
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-03-16 14:58:09 +0200

    1st task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
 
+refs.startBtn.addEventListener('click', onClickChangeColor);
+refs.stopBtn.addEventListener('click', onclickStopChangingColor);
+let timerId = null;
+refs.stopBtn.setAttribute('disabled', 'true');
+
+function onClickChangeColor(e) {
+    timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor()},1000);
+    refs.startBtn.setAttribute('disabled', 'true');
+    refs.stopBtn.removeAttribute("disabled");
+}
+
+
+function onclickStopChangingColor(e){
+  clearInterval(timerId);
+  refs.stopBtn.setAttribute('disabled', 'true');
+  refs.startBtn.removeAttribute("disabled");
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
