c1f3cd52a401108e1a6f08a85126b9b1b59ec8f1
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-03-20 10:22:23 +0200

    update 3d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,11 +17,9 @@ function onFormSubmit (e) {
 for (let position = 1; position <= amount; position +=1){
    createPromise(position, delay)
    .then(({position,delay}) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {useIcon: false});
    })
-   .catch((position,delay) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+   .catch(({position,delay}) => {
       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {useIcon: false});
    });
    delay += step;
