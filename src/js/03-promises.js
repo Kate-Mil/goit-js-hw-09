fe6d2843e7b3d25468393e009dab84d83be1d10f
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-03-20 10:15:11 +0200

    update 3d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,35 +15,31 @@ function onFormSubmit (e) {
   const amount = Number(e.target.amount.value); 
  
 for (let position = 1; position <= amount; position +=1){
-   console.log(position);
-   console.log('delay->',delay);
    createPromise(position, delay)
    .then(({position,delay}) => {
-    setTimeout(() => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {useIcon: false});
-    }, delay);
    })
    .catch((position,delay) => {
-    setTimeout (() => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {useIcon: false});
-    }, delay);
    });
    delay += step;
 }
 refs.form.reset();
 }
 
-
-
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  const objectPromise = { position, delay };
-
+  
   return new Promise((resolve, reject) => {
+  const shouldResolve = Math.random() > 0.3;
+
+  setTimeout(() => {
   if (shouldResolve) {
-    resolve(objectPromise);
+    resolve({position,delay});
   } else {
-    reject(objectPromise);
+    reject({position,delay});
   }
+  }, delay);
 });  
 }
